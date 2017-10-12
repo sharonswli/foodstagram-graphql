0ec92c8a747e2edb14704fd577a8377567fa33ed
Sharon Li <sharon.li@bluemesahealth.com>
2017-10-11 21:27:06 -0700

    Add comments. Define Meal and Restaurant types

--- server/data/schema.js
+++ server/data/schema.js
@@ -4,12 +4,38 @@ import {
 } from 'graphql-tools';
 import mocks from './mocks'
 
+// Our typeDef string defines our schema
+// Define your types within rootQuery that client is allowed to request
 const typeDefs = `
 type Query {
-  testString: String
+  meal(id: ID): Meal
+  author(firstName: String, lastName: String): Author
 }
+
+type Restaurant {
+  id: ID!
+  name: String
+  address: String
+  menu: [Meals]
+}
+
+type Meal {
+  id: ID!
+  description: String
+  catefory: String
+  calories: Number
+  image: String
+  servingAt: [Restaurant]
+}
+
+
+
+
 `;
 
+// TODO: Define resolvers
+
+// Make executable GraphQL schema from defined typeDefs
 const schema = makeExecutableSchema({ typeDefs });
 
 addMockFunctionsToSchema({ schema, mocks });
