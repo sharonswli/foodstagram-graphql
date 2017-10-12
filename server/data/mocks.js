53a7d9fbef5d17d97854010aa43f303b3aba445c
Sharon Li <sharon.li@bluemesahealth.com>
2017-10-11 21:48:14 -0700

    Add casual to generate mock data

--- server/data/mocks.js
+++ server/data/mocks.js
@@ -1,5 +1,24 @@
+import casual from 'casual';
+
 const mocks = {
-  String: () => 'Requesting mock success',
+  Query: () => ({
+    restaurant: (root, args) => {
+      return { name: args.name, address: args.address };
+    }
+  }),
+  Restaurant: () => ({ 
+    name: () => casual.company_name, 
+    address: () => casual.address,
+    menu: () => casual.array_of_words(8)
+  }),
+  Meal: () => ({
+    description: () => casual.description,
+    category: () => casual.word,
+    calories: () => casual.integer(0, 1000),
+    image: () => casual.uuid,
+    servingAt: () => casual.array_of_words(3)
+  })
+
 };
 
 export default mocks;
