9278e1907aaacdc627adcf5160f54696bad8abdb
Sharon Li <sharon.li@bluemesahealth.com>
2017-10-13 13:08:33 -0700

    Use sequelize functions in resolvers

--- server/data/resolvers.js
+++ server/data/resolvers.js
@@ -1,28 +1,27 @@
+import { User } from './connectors';
+
 const resolvers = {
   Query: {
-    user(root, args) {
-      return { id: 1, username: 'greg_likes_waffles' }
+    user(_, args) {
+      return User.find({ where: args });
     }
   },
   User: {
     posts(user) {
-      return [
-        { id: 1, userId: 1, description: 'd1', calories: 123, image: 'image.jpg', servingAt: 'JJ Diner' },
-        { id: 2, userId: 1, description: 'd2', calories: 234, image: 'image2.jpg', servingAt: 'ABC Diner' },
-        { id: 3, userId: 1, description: 'd2', calories: 234, image: 'image2.jpg', servingAt: 'ABC Diner' }
-      ];
+      return user.getPosts();
     }
   },
   Post: {
+    user(post) {
+      return post.getUser();
+    },
     servingAt(post) {
-      return { id: 1, name: 'JJ Diner', address: '123 Main Street, New York'}
+      return post.getRestaurant();
     }
   },
   Restaurant: {
     serving(restaurant) {
-      return [
-        { id: 1, description: 'd1', calories: 123, image: 'image.jpg', servingAt: 'JJ Diner' }
-      ]
+      return restaurant.getPosts();
     }
   }
 }
