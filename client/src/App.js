3c72cc83a9bfb5966d6540255b9a4d4259318c70
Sharon Li <sharon.li@bluemesahealth.com>
2017-10-13 23:04:15 -0700

    Client: move query to app level. Pass down to FoodList as props

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,74 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { graphql, gql } from 'react-apollo';
+import PropTypes from 'prop-types';
+
 import FoodList from './components/FoodList';
 
+//@graphql(AppQuery)
+//@graphql(AppMutation) 
 class App extends Component {
 
+  // Define propTypes
+  static propTypes = {
+    data: PropTypes.shape({
+      loading: PropTypes.bool,
+      error: PropTypes.object,
+      user: PropTypes.object
+    })
+  }
+  
   render() {
+    // Check if data is loading
+    if(this.props.data.loading) {
+      return (<div>Loading...</div>)
+    }
+
+    if(this.props.data.error) {
+      console.error(this.props.data.error)
+      return (<div>An unexpected error occurred. Please contact support</div>)
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Foodstagram</h1>
         </header>
-          <FoodList/>
+
+        <section className="user-info">
+          <p>Hello {this.props.data.user.firstName}! </p>
+          <img src={this.props.data.user.avatar} />          
+        </section>
+
+        <section>
+          <FoodList food={this.props.data.user.posts}/>
+        </section>
       </div>
     );
   }
 }
-export default App;
 
-// export default graphql(AppQuery)(App);
+const AppQuery = gql`
+query AppQuery($userId: ID!) {
+  user(id: $userId) {
+    firstName
+    avatar
+    posts {
+      id
+      image
+      description
+    }
+  }
+}
+`
+
+const AppWithData = graphql(AppQuery, { 
+  options : {
+    variables: {
+      userId: 1
+    }
+  }
+})(App);
+export default AppWithData;
+
--- client/src/components/FoodList.jsx
+++ client/src/components/FoodList.jsx
@@ -3,58 +3,20 @@ import { graphql, gql } from 'react-apollo';
 import PropTypes from 'prop-types';
 import FoodItem from './FoodItem';
 
-// @graphql(FoodListQuery)
-// @graphql(FoodListMutation)
 class FoodList extends Component {
   
-  // Define propTypes
-  static propTypes = {
-    data: PropTypes.shape({
-      loading: PropTypes.bool,
-      error: PropTypes.object,
-      user: PropTypes.object
-    })
+  constructor(props) {
+    super(props)
   }
 
-  render() {
-    // Check if data is loading
-    if(this.props.data.loading) {
-      return (<div>Loading...</div>)
-    }
-
-    if(this.props.data.error) {
-      console.error(this.props.data.error)
-      return (<div>An unexpected error occurred. Please contact support</div>)
-    }
-    
+  render() {    
     return (
       <div className="food-list-container">
-        <div className="user-info">
-          <p>Hello {this.props.data.user.firstName}! </p>
-          <img src={this.props.data.user.avatar} />          
-        </div>
         <div className="food-item-container">
-          { this.props.data.user.posts.map(post => <FoodItem key={post.id} post={post}/>)}
+          { this.props.food.map(post => <FoodItem key={post.id} post={post}/>)}
         </div>
       </div>
     )
   }
 }
-
-const FoodListQuery = gql`
-  query FoodList {
-    user(id: 1) {
-      firstName
-      avatar
-      posts {
-        id
-        image
-        description
-      }
-    }
-  }
-`
-
-const FoodListWithData = graphql(FoodListQuery)(FoodList)
-
-export default FoodListWithData
+export default FoodList
